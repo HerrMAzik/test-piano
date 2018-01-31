e51f286e5c2a8ae8e45b48197ef11f682a8235dd
Azat_Mirzaianov <Azat_Mirzaianov@epam.com>
2018-01-31 15:27:47 +0400

    Added explicit version of dependencies

--- src/main/java/com/home/project/config/TemplateEngineConfig.java
+++ src/main/java/com/home/project/config/TemplateEngineConfig.java
@@ -1,5 +1,6 @@
 package com.home.project.config;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
@@ -9,5 +10,10 @@ public class TemplateEngineConfig {
     @Bean
     public Java8TimeDialect java8TimeDialect() {
         return new Java8TimeDialect();
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 }
